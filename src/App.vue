a2ba0b8365c98228d2f15a0ab5b000b2f0202a53
azkifairuz <azkiajmal@gmail.com>
2024-01-30 19:38:31 +0700

    feat navbar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,16 @@
 <script setup>
+import { RouterView } from 'vue-router';
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <nav class="w-full flex justify-center items-center bg-[#dd2d4a] p-5">
+    <div class="flex gap-4 items-center">
+      <!-- <img class="h-16" src="./assets/logo_mu.png" alt=""> -->
+      <h1 class="font-poppins text-white font-semibold text-xl">halobunda</h1>
+    </div>
+  </nav>
+  <RouterView></RouterView>  
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
